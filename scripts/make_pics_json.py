7d3ef7d1b0321f8193bef908e0c69421daf2537d
Matt Wiens <mwiens91@gmail.com>
2021-10-07 14:36:25 -0400

    Add image size info to JSON generating script

--- scripts/make_pics_json.py
+++ scripts/make_pics_json.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+from PIL import Image
 
 PICS_DIR = "../pics"
 THUMBS_DIR = "../pics/thumbs"
@@ -11,8 +12,29 @@ all_thumbs = sorted(os.listdir(THUMBS_DIR), reverse=True)
 
 pics_list = []
 
-for p, t in zip(all_pics, all_thumbs):
-    pics_list.append({"src": os.path.join("/", p), "srct": os.path.join("/thumbs/", t)})
+for pic, thumb in zip(all_pics, all_thumbs):
+    # Get dimensions
+    pic_path = os.path.join(PICS_DIR, pic)
+    thumb_path = os.path.join(THUMBS_DIR, pic)
+
+    with Image.open(pic_path) as im:
+        pic_w, pic_h = im.size
+
+    with Image.open(thumb_path) as im:
+        thumb_w, thumb_h = im.size
+
+    # Put everything relevant in the list
+    pics_list.append(
+        {
+            "kind": "image",
+            "height": pic_h,
+            "width": pic_w,
+            "imgtHeight": thumb_h,
+            "imgtWidth": thumb_w,
+            "src": os.path.join("/", pic),
+            "srct": os.path.join("/thumbs/", thumb),
+        }
+    )
 
 pics_json = json.dumps(pics_list, indent=2)
 
