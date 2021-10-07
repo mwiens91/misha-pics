de67cd44821f12870ad3814d6885169cfa443bbf
Matt Wiens <mwiens91@gmail.com>
2021-10-07 14:27:19 -0400

    Clean up the thumbnail script

--- scripts/make_thumbs.py
+++ scripts/make_thumbs.py
@@ -9,16 +9,15 @@ PICS_DIR = "../pics"
 THUMBS_DIR = "../pics/thumbs"
 
 # Set this to True if you want to do a clean sweep of the thumbnails
-rewrite_existing = False
+REWRITE_EXISTING = False
 
 all_pics = [f for f in os.listdir(PICS_DIR) if f[-3:] == "jpg"]
-all_thumbs = os.listdir(THUMBS_DIR)
 
 for pic in all_pics:
     pic_path = os.path.join(PICS_DIR, pic)
     thumb_path = os.path.join(THUMBS_DIR, pic)
 
-    if not os.path.isfile(thumb_path) or rewrite_existing:
+    if not os.path.isfile(thumb_path) or REWRITE_EXISTING:
         with Image.open(pic_path) as raw_im:
             with ImageOps.exif_transpose(raw_im) as im:
                 w, h = im.size
