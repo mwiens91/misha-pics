664cbb7a33d4c2390a7c652b66ef6d0203c7fae7
Matt Wiens <mwiens91@gmail.com>
2021-10-07 01:47:24 -0400

    Remove removing quotes from JSON

--- scripts/make_pics_json.py
+++ scripts/make_pics_json.py
@@ -16,7 +16,6 @@ for p, t in zip(all_pics, all_thumbs):
     pics_list.append({"src": p, "srct": os.path.join("/thumbs/", t)})
 
 pics_json = json.dumps(pics_list, indent=2)
-pics_json_no_key_quotes = re.sub(r'"(.*?)"(?=:)', r"\1", pics_json)
 
 # Pipe this to a file
-print(pics_json_no_key_quotes)
+print(pics_json)
