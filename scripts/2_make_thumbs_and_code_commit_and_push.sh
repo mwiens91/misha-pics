7e59b1f381093c46d1065d18792f61f1e3810ee4
Matt Wiens <mwiens91@gmail.com>
2021-10-08 13:13:45 -0400

    Make script to push both repos

---
+++ scripts/2_make_thumbs_and_code_commit_and_push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+./1_make_thumbs_and_code.sh
+
+git add ../pics
+git commit -m "Add new cat pics"
+
+git -C ../../misha add js/gallery.js
+git -C ../../misha commit -m "Add new cat pics"
+
+git push
+git -C ../../misha push
